f0fc8713f5367fe8061e38e8221cc4ab77eef075
Matthew Phillips <phillips.matthew829@gmail.com>
2022-06-17 12:50:15 -0500

    commit tree ecf1e66780d55b580c18226896e5b322dde877ff

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ApplicationRecord
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+
+    # Validate email to be uniquely case-sensitive
 end
